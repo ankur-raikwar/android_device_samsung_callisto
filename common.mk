dce1803e4f63f65072f2099165bd32f7118b9b70
Ankur Raikwar <ankur5technical@gmail.com>
2013-09-30 16:37:24 +0530

    necessary change
    *Build.prop overrides.
    *Enable HW Composer
    *Bluetooth
    *USB for ramdisk

--- common.mk
+++ common.mk
@@ -20,10 +20,46 @@ PRODUCT_PACKAGES += \
     audio_policy.msm7x27 \
     audio.primary.msm7x27
 
+# BlueZ: binaries
+PRODUCT_PACKAGES += \
+    bluetoothd \
+    brcm_patchram_plus \
+    libbluetoothd \
+    hcitool \
+    hciconfig \
+    hciattach
+
+# BlueZ: configs
+PRODUCT_COPY_FILES += \
+    system/bluetooth/data/audio.conf:system/etc/bluetooth/audio.conf \
+    system/bluetooth/data/auto_pairing.conf:system/etc/bluetooth/auto_pairing.conf \
+    system/bluetooth/data/blacklist.conf:system/etc/bluetooth/blacklist.conf \
+    system/bluetooth/data/input.conf:system/etc/bluetooth/input.conf \
+    system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf \
+    system/bluetooth/data/network.conf:system/etc/bluetooth/network.conf
+    system/bluetooth/data/stack.conf:system/etc/bluetooth/stack.conf
+
+# BlueZ: javax.btobex is required by Bluetooth_msm
+PRODUCT_PACKAGES += \
+    javax.btobex
+
+# BlueZ: properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qc.bluetooth.stack=bluez \
+    ro.qualcomm.bluetooth.dun=true \
+    ro.qualcomm.bluetooth.ftp=true \
+    ro.qualcomm.bluetooth.hfp=true \
+    ro.qualcomm.bluetooth.hsp=true \
+    ro.qualcomm.bluetooth.map=true \
+    ro.qualcomm.bluetooth.nap=true \
+    ro.qualcomm.bluetooth.opp=true \
+    ro.qualcomm.bluetooth.pbap=true \
+    ro.qualcomm.bluetooth.sap=true
+
 ## Camera
 PRODUCT_PACKAGES += \
-    libcamera \
-    LegacyCamera
+    camera.msm7x27 \
+    libcamera
 
 # FM Radio
 PRODUCT_PACKAGES += \
@@ -35,12 +71,13 @@ PRODUCT_COPY_FILES += \
 
 ## GPS
 PRODUCT_PACKAGES += \
+    gps.msm7x27 \
     librpc
 
 ## HW Composer
 PRODUCT_PACKAGES += \
     hwcomposer.msm7x27
-
+    
 ## Other
 PRODUCT_PACKAGES += \
     lights.msm7x27 \
@@ -48,10 +85,6 @@ PRODUCT_PACKAGES += \
     make_ext4fs \
     brcm_patchram_plus \
     setup_fs
-
-## Vold config
-PRODUCT_COPY_FILES += \
-    device/samsung/callisto/prebuilt/etc/vold.fstab:system/etc/vold.fstab
 
 ## Hardware properties
 PRODUCT_COPY_FILES += \
@@ -64,39 +97,90 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 ## Wi-Fi & networking
 PRODUCT_COPY_FILES += \
-    device/samsung/callisto/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-    device/samsung/callisto/prebuilt/etc/wifi/hostapd.conf:system/etc/wifi/hostapd.conf \
-    device/samsung/callisto/prebuilt/etc/wifi/dhcpcd.conf:system/etc/wifi/dhcpcd.conf \
-    device/samsung/callisto/prebuilt/etc/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
-    device/samsung/callisto/prebuilt/bin/get_macaddrs:system/bin/get_macaddrs
+    device/samsung/cooper/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/samsung/cooper/prebuilt/etc/wifi/hostapd.conf:system/etc/wifi/hostapd.conf \
+    device/samsung/cooper/prebuilt/bin/get_macaddrs:system/bin/get_macaddrs
 
 ## Media
 PRODUCT_COPY_FILES += \
-    device/samsung/callisto/prebuilt/etc/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
-    device/samsung/callisto/prebuilt/etc/AudioFilter.csv:system/etc/AudioFilter.csv \
-    device/samsung/callisto/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
-    device/samsung/callisto/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
-    device/samsung/callisto/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml
+    device/samsung/cooper/prebuilt/etc/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
+    device/samsung/cooper/prebuilt/etc/AudioFilter.csv:system/etc/AudioFilter.csv \
+    device/samsung/cooper/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
+    device/samsung/cooper/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    device/samsung/cooper/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml
+
+## Keymap
+PRODUCT_COPY_FILES += \
+    device/samsung/cooper/prebuilt/usr/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
+    device/samsung/cooper/prebuilt/usr/keylayout/sec_key.kl:system/usr/keylayout/sec_key.kl
+
+## Keychar
+PRODUCT_COPY_FILES += \
+    device/samsung/cooper/prebuilt/usr/keylayout/qwerty.kcm:system/usr/keylayout/qwerty.kcm \
+    device/samsung/cooper/prebuilt/usr/keylayout/qwerty2.kcm:system/usr/keylayout/qwerty2.kcm \
+    device/samsung/cooper/prebuilt/usr/keylayout/Virtual.kcm:system/usr/keylayout/Virtual.kcm \
+    device/samsung/cooper/prebuilt/usr/keylayout/Generic.kcm:system/usr/keylayout/Generic.kcm
 
 ## Touchscreen
 PRODUCT_COPY_FILES += \
-    device/samsung/callisto/prebuilt/usr/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
+    device/samsung/cooper/prebuilt/usr/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
 
 # GPS conf
 PRODUCT_COPY_FILES += \
-    device/samsung/callisto/prebuilt/etc/gps.conf:system/etc/gps.conf
+    device/samsung/cooper/prebuilt/etc/gps.conf:system/etc/gps.conf
+
+# BEGIN: Common properties
+
+## Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.checkjni=0 \
+    dalvik.vm.debug.alloc=0 \
+    dalvik.vm.dexopt-data-only=1 \
+    dalvik.vm.dexopt-flags=v=a,o=v,m=y,u=y
+
+## Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=mdp \
+    debug.sf.no_hw_vsync=0
 
 ## Loop ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.call_ring.multiple=false \
-    ro.telephony.call_ring.delay=3000
+    ro.telephony.call_ring.delay=3000 \
+    ro.telephony.call_ring.multiple=false
 
-## Ramdisk
+## Other
+PRODUCT_PROPERTY_OVERRIDES += \
+    DEVICE_PROVISIONED=1 \
+    dev.sfbootcomplete=0 \
+    ro.config.play.bootsound=0 \
+    ro.setupwizard.enable_bypass=1
+
+## RIL, telephony
+PRODUCT_PROPERTY_OVERRIDES += \
+    mobiledata.interfaces=pdp0,gprs,ppp0 \
+    rild.libargs=-d/dev/smd0 \
+    rild.libpath=/system/lib/libsec-ril.so \
+    ro.telephony.default_network=0 \
+    ro.telephony.ril_class=SamsungMSMRIL
+
+## USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.service.adb.enable=1 \
+    persist.sys.usb.config=mass_storage,adb
+
+## WiFi
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.ap.interface=athap0 \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=180
+
+# Ramdisk
 ## SAMSUNG_BOOTLOADER is the product model changed into appropriate string parsed by init.
 ## Example: -GT-I5500 becomes gt-i5500board, -GT-S5830 becomes gt-s5830board, and so on.
 SAMSUNG_BOOTLOADER := $(shell echo $(PRODUCT_VERSION_DEVICE_SPECIFIC)board | tr '[A-Z]' '[a-z]' | cut -c 2-)
@@ -104,8 +188,8 @@ SAMSUNG_BOOTLOADER := $(shell echo $(PRODUCT_VERSION_DEVICE_SPECIFIC)board | tr 
 PRODUCT_PACKAGES += \
     fstab.$(SAMSUNG_BOOTLOADER) \
     init.$(SAMSUNG_BOOTLOADER).rc \
-    ueventd.$(SAMSUNG_BOOTLOADER).rc \
-    init.$(SAMSUNG_BOOTLOADER).usb.rc
+    init.$(SAMSUNG_BOOTLOADER).usb.rc \
+    ueventd.$(SAMSUNG_BOOTLOADER).rc
 
 # Inherit qcom/msm7x27
 $(call inherit-product, device/qcom/msm7x27/msm7x27.mk)
@@ -114,13 +198,7 @@ $(call inherit-product, device/qcom/msm7x27/msm7x27.mk)
 $(call inherit-product, vendor/google/gapps_armv6_tiny.mk)
 
 # Common assets
-PRODUCT_AAPT_CONFIG := normal ldpi mdpi
-PRODUCT_LOCALES += en
+PRODUCT_AAPT_CONFIG := ldpi mdpi normal
 
-# Samsung callisto overlays
+# Samsung msm7x27-common overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/callisto/overlay
-
-# Build.prop overrides
-PRODUCT_PROPERTY_OVERRIDES += \
-    hwui.render_dirty_regions=false \
-    pm.sleep_mode=1
